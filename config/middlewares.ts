e5273c72cf36e4218cd24a762f293ab0ba448f53
Yves-Developer <yvesmugisha09@gmail.com>
2025-03-12 15:11:29 +0200

    ## Cloudinary Image Upload, configured

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,7 +1,23 @@
 export default [
   'strapi::logger',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:', 'http://localhost:8080', 'ws://localhost:8080', 'res.cloudinary.com'],
+          'img-src': ["'self'", 'data:', 'blob:', 'https://market-assets.strapi.io', 'https://res.cloudinary.com'],
+          'media-src': ["'self'", 'data:', 'blob:', 'https://market-assets.strapi.io', 'https://res.cloudinary.com'],
+          'script-src': ["'self'", "'unsafe-inline'", "'unsafe-eval'"], // Allow inline scripts
+          'frame-src': ["'self'", 'https://res.cloudinary.com'],
+          'worker-src': ["'self'", 'blob:'],
+        },
+      },
+    },
+  },
+
   'strapi::errors',
-  'strapi::security',
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::query',
