f52804abbee6ba1b2c525d5301ed06e6626f16f6
Yves-Developer <yvesmugisha09@gmail.com>
2025-09-14 23:12:54 +0200

    Resend Email Configuratio [STRAPI_CMS]

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,19 +1,32 @@
-export default ({env}) => ({
-    // ...
-    upload: {
-      config: {
-        provider: 'cloudinary',
-        providerOptions: {
-          cloud_name: env('CLOUDINARY_NAME'),
-          api_key: env('CLOUDINARY_KEY'),
-          api_secret: env('CLOUDINARY_SECRET'),
-        },
-        actionOptions: {
-          upload: {},
-          uploadStream: {},
-          delete: {},
-        },
+export default ({ env }) => ({
+  // Upload config , provider: Cloudinary
+  upload: {
+    config: {
+      provider: "cloudinary",
+      providerOptions: {
+        cloud_name: env("CLOUDINARY_NAME"),
+        api_key: env("CLOUDINARY_KEY"),
+        api_secret: env("CLOUDINARY_SECRET"),
+      },
+      actionOptions: {
+        upload: {},
+        uploadStream: {},
+        delete: {},
       },
     },
-    // ...
-  });
+  },
+
+  // Email Config, Provider: Resend
+  email: {
+    config: {
+      provider: "strapi-provider-email-resend-strapi",
+      providerOptions: {
+        apiKey: env("RESEND_API_KEY"), // Required
+      },
+      settings: {
+        defaultFrom: "admin@noreply.yvesdc.site", // env('RESEND_DEFAULT_FROM')
+        defaultReplyTo: "admin@noreply.yvesdc.site", // env('RESEND_DEFAULT_FROM')
+      },
+    },
+  },
+});
